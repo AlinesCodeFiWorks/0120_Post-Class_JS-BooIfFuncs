7e6404690a819590e8ac0d9532900df174fe266f
Aline Yui Taylor <alineyui.taylor@gmail.com>
2025-01-20 18:35:43 -0600

    Completed class notes through functions

--- js-boolean-if-statements-functions/script.js
+++ js-boolean-if-statements-functions/script.js
@@ -20,3 +20,67 @@ console.log(Boolean("hello")); //true
 console.log(Boolean(null)); //false
 console.log(Boolean(undefined)); //false
 console.log(Boolean(NaN)); //false
+
+//if statements
+
+let age;
+age = 22;
+if (age >= 18) {
+  console.log("you are able to vote");
+} else {
+  console.log("you're too young!");
+}
+
+//Combining conditionals and logical operators
+// OR operator
+let isWeekend = true;
+let isHoliday = false;
+
+if (isWeekend || isHoliday) {
+  console.log("you can relax today!"); //this will be run
+} else {
+  console.log("it's a workday");
+}
+
+// AND operator
+isWeekend = true;
+isHoliday = false;
+
+if (isWeekend && isHoliday) {
+  console.log("you can relax today!");
+} else {
+  console.log("it's a workday"); //this will be run
+}
+
+//practice
+
+let rain = true;
+let cold = false;
+
+if (rain && cold) {
+  console.log("Bundle up and don't forget your umbrella!");
+} else if (rain || cold) {
+  console.log("Check the weather: it may be cold or ranining!");
+} else {
+  console.log("Enjoy the nice weather!");
+}
+
+//Nested if-statements
+const username = "admin";
+const password = "password123";
+
+if (username === "admin") {
+  if (password === "password123") {
+    console.log("Access granted!");
+  } else {
+    console.log("Incorrect password.");
+  }
+} else {
+  console.log("unknown user");
+}
+
+//functions
+function greet(firstName, lastName) {
+  console.log(`Hello, ${firstName} ${lastName}!`);
+}
+greet("Alice", "CodeLabs");
